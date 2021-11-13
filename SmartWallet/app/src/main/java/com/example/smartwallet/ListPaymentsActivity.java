5156c5a08d71d20d32886398c36d6bb0c173f2b9
doritmtm <dorian_tm@yahoo.com>
2021-11-14 01:15:50 +0200

    Tweaked card layout a little

--- SmartWallet/app/src/main/java/com/example/smartwallet/ListPaymentsActivity.java
+++ SmartWallet/app/src/main/java/com/example/smartwallet/ListPaymentsActivity.java
@@ -6,6 +6,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 
