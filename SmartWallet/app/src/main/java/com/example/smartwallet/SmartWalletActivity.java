5fc0f6fb94d6659d582ae8349bf2d31b05c882ae
doritmtm <dorian_tm@yahoo.com>
2021-11-20 20:55:31 +0200

    Added automatic update of values and months

--- SmartWallet/app/src/main/java/com/example/smartwallet/SmartWalletActivity.java
+++ SmartWallet/app/src/main/java/com/example/smartwallet/SmartWalletActivity.java
@@ -19,8 +19,10 @@ import com.example.smartwallet.models.MonthlyExpenses;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -38,31 +40,28 @@ public class SmartWalletActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        monthText=findViewById(R.id.monthText);
-        updateBut=findViewById(R.id.updateButton);
-        incomeInput=findViewById(R.id.incomeInput);
-        expensesInput=findViewById(R.id.expensesInput);
-        monthSpinner=findViewById(R.id.monthSpinner);
-        monthsAdapter=new ArrayAdapter(this, android.R.layout.simple_spinner_item);
+        monthText = findViewById(R.id.monthText);
+        updateBut = findViewById(R.id.updateButton);
+        incomeInput = findViewById(R.id.incomeInput);
+        expensesInput = findViewById(R.id.expensesInput);
+        monthSpinner = findViewById(R.id.monthSpinner);
+        monthsAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item);
         monthsAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         monthSpinner.setAdapter(monthsAdapter);
         monthSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                month=monthsAdapter.getItem(position).toString();
+                month = monthsAdapter.getItem(position).toString();
                 dbref.child("calendar").child(month).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
                     @Override
                     public void onComplete(@NonNull Task<DataSnapshot> task) {
-                        mexpense =task.getResult().getValue(MonthlyExpenses.class);
-                        if(mexpense!=null)
-                        {
+                        mexpense = task.getResult().getValue(MonthlyExpenses.class);
+                        if (mexpense != null) {
                             monthText.setText(month);
                             incomeInput.setText(Float.toString(mexpense.getIncome()));
                             expensesInput.setText(Float.toString(mexpense.getExpenses()));
-                        }
-                        else
-                        {
-                            monthText.setText("Error finding "+month+" in database");
+                        } else {
+                            monthText.setText("Error finding " + month + " in database");
                         }
                     }
                 });
@@ -73,19 +72,29 @@ public class SmartWalletActivity extends AppCompatActivity {
 
             }
         });
-        dbref= FirebaseDatabase.getInstance("https://smart-wallet-30b48-default-rtdb.europe-west1.firebasedatabase.app").getReference();
+        dbref = FirebaseDatabase.getInstance("https://smart-wallet-30b48-default-rtdb.europe-west1.firebasedatabase.app").getReference();
         dbref.child("calendar").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
             @Override
             public void onComplete(@NonNull Task<DataSnapshot> task) {
-                DataSnapshot snap=task.getResult();
-                DataSnapshot itsnap;
-                Iterator<DataSnapshot> it=snap.getChildren().iterator();
-                monthsAdapter.clear();
-                while(it.hasNext())
+                DataSnapshot snap = task.getResult();
+                updateMonthsFromDB(snap);
+            }
+        });
+        dbref.child("calendar").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                updateMonthsFromDB(snapshot);
+                if(mexpense!=null && month!=null)
                 {
-                    itsnap=it.next();
-                    monthsAdapter.add(itsnap.getKey());
+                    mexpense = snapshot.child(month).getValue(MonthlyExpenses.class);
+                    incomeInput.setText(Float.toString(mexpense.getIncome()));
+                    expensesInput.setText(Float.toString(mexpense.getExpenses()));
                 }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
             }
         });
     }
@@ -107,6 +116,17 @@ public class SmartWalletActivity extends AppCompatActivity {
                     }
                 }
                 break;
+        }
+    }
+
+    private void updateMonthsFromDB(DataSnapshot snap) {
+        DataSnapshot itsnap;
+        Iterator<DataSnapshot> it= snap.getChildren().iterator();
+        monthsAdapter.clear();
+        while(it.hasNext())
+        {
+            itsnap=it.next();
+            monthsAdapter.add(itsnap.getKey());
         }
     }
 }
