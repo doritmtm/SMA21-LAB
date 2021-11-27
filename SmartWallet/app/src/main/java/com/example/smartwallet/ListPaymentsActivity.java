150775f9fd50fef77d9b3f14ea892e39d811387f
doritmtm <dorian_tm@yahoo.com>
2021-11-27 20:43:20 +0200

    Payments are now filtered by month

--- SmartWallet/app/src/main/java/com/example/smartwallet/ListPaymentsActivity.java
+++ SmartWallet/app/src/main/java/com/example/smartwallet/ListPaymentsActivity.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
@@ -32,9 +34,11 @@ public class ListPaymentsActivity extends AppCompatActivity {
     private Button leftButton,rightButton;
     private TextView monthText,statusText;
     private RecyclerView paymentsList;
-    private int month;
     private DatabaseReference dbref;
     private List<Payment> paymentDataList=new ArrayList<>();
+    private int currentMonth;
+    private PaymentListAdapter paymentAdapter=new PaymentListAdapter();
+    private SharedPreferences pref;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,7 +48,8 @@ public class ListPaymentsActivity extends AppCompatActivity {
         monthText=findViewById(R.id.monthText2);
         statusText=findViewById(R.id.statusText);
         paymentsList=findViewById(R.id.paymentsList);
-        month=3;
+        pref=getPreferences(Context.MODE_PRIVATE);
+        currentMonth=pref.getInt("currentMonth",0);
         PaymentListAdapter paymentAdapter=new PaymentListAdapter();
         paymentsList.setAdapter(paymentAdapter);
         LinearLayoutManager listMgr=new LinearLayoutManager(this);
@@ -53,7 +58,7 @@ public class ListPaymentsActivity extends AppCompatActivity {
         dbref.child("wallet").addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                PaymentListAdapter paymentAdapter=new PaymentListAdapter();
+
                 Iterator<DataSnapshot> it=snapshot.getChildren().iterator();
                 DataSnapshot datasnap;
                 Payment payment;
@@ -65,7 +70,13 @@ public class ListPaymentsActivity extends AppCompatActivity {
                     payment.setDate(datasnap.getKey());
                     paymentDataList.add(payment);
                 }
-                paymentAdapter.setPaymentDataList(paymentDataList);
+                if(currentMonth==0 && paymentDataList.size()>0)
+                {
+                    currentMonth=Integer.parseInt(paymentDataList.get(0).getDate().substring(5,7));
+                    pref.edit().putInt("currentMonth",currentMonth).apply();
+                }
+                paymentAdapter.setPaymentDataList(filterDBPayments(currentMonth));
+                monthText.setText("Payments from "+getMonthName(currentMonth));
                 statusText.setText("Success!");
                 paymentsList.setAdapter(paymentAdapter);
             }
@@ -75,7 +86,6 @@ public class ListPaymentsActivity extends AppCompatActivity {
 
             }
         });
-        monthText.setText("All Payments");
     }
 
     public void clicked(View view)
@@ -87,6 +97,61 @@ public class ListPaymentsActivity extends AppCompatActivity {
                 Intent intent=new Intent(this,AddPaymentActivity.class);
                 startActivity(intent);
                 break;
+            case R.id.leftButton:
+                currentMonth--;
+                if(currentMonth<=0)
+                {
+                    currentMonth=12;
+                }
+                pref.edit().putInt("currentMonth",currentMonth).apply();
+                paymentAdapter.setPaymentDataList(filterDBPayments(currentMonth));
+                paymentsList.setAdapter(paymentAdapter);
+                monthText.setText("Payments from "+getMonthName(currentMonth));
+                break;
+            case R.id.rightButton:
+                currentMonth++;
+                if(currentMonth>=13)
+                {
+                    currentMonth=1;
+                }
+                pref.edit().putInt("currentMonth",currentMonth).apply();
+                paymentAdapter.setPaymentDataList(filterDBPayments(currentMonth));
+                paymentsList.setAdapter(paymentAdapter);
+                monthText.setText("Payments from "+getMonthName(currentMonth));
+                break;
         }
+    }
+
+    private ArrayList<Payment> filterDBPayments(int month)
+    {
+        ArrayList<Payment> paymentsList=new ArrayList<>();
+        for(Payment p:paymentDataList)
+        {
+            if(Integer.parseInt(p.getDate().substring(5,7))==month)
+            {
+                paymentsList.add(p);
+            }
+        }
+        return paymentsList;
+    }
+
+    private String getMonthName(int month)
+    {
+        switch(month)
+        {
+            case 1: return "January";
+            case 2: return "February";
+            case 3: return "March";
+            case 4: return "April";
+            case 5: return "May";
+            case 6: return "June";
+            case 7: return "July";
+            case 8: return "August";
+            case 9: return "September";
+            case 10: return "October";
+            case 11: return "November";
+            case 12: return "December";
+        }
+        return "Error";
     }
 }
